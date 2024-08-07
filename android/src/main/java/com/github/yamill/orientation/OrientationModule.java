bebf6779975281142318513c780de850211a1232
Ahmet Ardal <ardalahmet@gmail.com>
2024-08-07 23:55:17 +0300

    Fix registerReceiver call for API 34

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -216,7 +216,12 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
             FLog.e(ReactConstants.TAG, "no activity to register receiver");
             return;
         }
-        activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
+
+        if (Build.VERSION.SDK_INT >= 34 && activity.getApplicationInfo().targetSdkVersion >= 34) {
+            activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"), Context.RECEIVER_EXPORTED);
+        } else {
+            activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
+        }
     }
 
     @Override
